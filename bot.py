b5d00fb475a29738e10116b1207b42a6672cccf9
Ekter <ektermoi1@gmail.com>
2022-08-31 18:19:57 +0200

    change token

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ from discord.ext import commands
 
 with open("whatisthis.txt","r") as f:
     token=base64.b64decode(f.readline())
-print(base64.b64encode(b'MTAxNDU2MjU3ODA3ODM4NDI3MA.GIjuF7.2bB27xREtsbl-BkToQCl2eWRzB8SM1Dw094Jfw').decode('utf-8'),end="")
-bot = commands.Bot(command_prefix=["§", "rb"])
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user.name} has connected to Discord!")
-
-
-bot.run(token)
+print(base64.b64encode(b'MTAxNDU2MjU3ODA3ODM4NDI3MA.GEwkDF.Bpn4zjDHXFCfTmLFqZSdc6Tbm1C6fvaR9J9rpE').decode('utf-8'),end="")
+# bot = commands.Bot(command_prefix=["§", "rb"])
+# 
+# @bot.event
+# async def on_ready():
+    # print(f"{bot.user.name} has connected to Discord!")
+# 
+# 
+# bot.run(token)
