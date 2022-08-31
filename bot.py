c99a4037d5c02feea3ef85a2de4850fbcd73b053
Ekter <ektermoi1@gmail.com>
2022-08-31 18:23:37 +0200

    a

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ from discord.ext import commands
 
 with open("whatisthis.txt","r") as f:
     token=base64.b64decode(f.readline())
-# bot = commands.Bot(command_prefix=["§", "rb"])
-# 
-# @bot.event
-# async def on_ready():
-    # print(f"{bot.user.name} has connected to Discord!")
-# 
-# 
-# bot.run(token)
+bot = commands.Bot(command_prefix=["§", "rb"])
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} has connected to Discord!")
+
+
+bot.run(token)
