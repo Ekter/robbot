0cac2181744d95bd530a283deacd8bc50141049b
Ekter <ektermoi1@gmail.com>
2022-08-31 18:21:19 +0200

    other token change :))))))

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 
 with open("whatisthis.txt","r") as f:
     token=base64.b64decode(f.readline())
-print(base64.b64encode(b'MTAxNDU2MjU3ODA3ODM4NDI3MA.GEwkDF.Bpn4zjDHXFCfTmLFqZSdc6Tbm1C6fvaR9J9rpE').decode('utf-8'),end="")
 # bot = commands.Bot(command_prefix=["§", "rb"])
 # 
 # @bot.event
